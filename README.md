87167465bc8bba1e2ab8e14283e5ae4c994747d0
TeddyBear526 <theodor.bygdemark@elev.ga.ntig.se>
2026-09-24 10:42:07 +0200

    monster class made

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Planera-och-Utf-ra-
 Monster spel där jag kommer göra en komplett planering före koden
--- game.py
+++ game.py
@@ -1,1 +1,11 @@
+class monster:
+    def __init__(self,type,attack,HP):
+        self.__type = type
+        self.__attack = attack
+        self.__HP = HP
 
+    def take_dmg(self, attack):
+        self.__HP -= attack
+
+    def deal_dmg(self):
+        return self.__attack
